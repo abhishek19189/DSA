d2dcec2c0464b94534c82238af5b63aeb265bbea
infernal <abhishek.cc241002@global.org.in>
2025-09-03 07:32:34 +0530

    switch

--- lec5/loop1.c
+++ lec5/loop1.c
@@ -29,16 +29,7 @@
 //     return 0;
 // }
 
-// #include<stdio.h>
-// int main(){
-//     int sum =0;
-//     for(int i = 1; i<=10;i++)
-//     {
-//         sum=sum+i;
-//         // printf("%d \n" ,sum);
-//     } printf("%d \n" ,sum);
-// 
-//}
+
 
 #include <stdio.h>
 
