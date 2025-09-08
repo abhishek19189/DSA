9c7c6f276cf9fb3fddb4360aedcdc82fcad377fa
infernal <abhishek.cc241002@global.org.in>
2025-09-08 14:19:34 +0530

    nasted

---
+++ lec8/pattern.c
@@ -0,0 +1,50 @@
+// #include<stdio.h>
+// int main(){
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j<=i;j++)
+//         {
+//             printf(" * ");
+//         }
+//         printf("\n");
+//     }
+    
+// }
+
+
+
+
+
+
+
+// #include<stdio.h>
+// int main(){
+//     for (int i=5; i>0; i--){
+//         for (int j = 0; j<i;j++){
+        
+//             printf(" * ");
+//         }
+//         printf("\n");
+//     }
+//     }
+    
+
+#include<stdio.h>
+int main(){
+    for (int i=0; i<3; i++){
+        for (int j = 0; j<=1; j++)
+        {
+            printf("__");
+        }
+        
+        for (int k = 3; k>i ; k--)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+    }
+    
+
+
